c450c6d8b7a52742f1398ebe708c632cb2a36403
Liwen Dai <lwdai1990@gmail.com>
2017-12-02 19:57:02 -0500

    auction simulation

--- testGM.py
+++ testGM.py
@@ -34,8 +34,8 @@ def test_gm_enc_dec(iters = 1):
         cipher = encrypt_gm(num, n)
         
         # ReEncryption
-        for j in range(3):
-            cipher = [c * encrypt_gm(0, n)[0] % n for c in cipher ]
+        #for j in range(3):
+        #    cipher = [c * encrypt_gm(0, n)[0] % n for c in cipher ]
         
         decrypted = decrypt_gm(cipher, (p,q))
         
@@ -62,7 +62,7 @@ def test_gm_homo(iters = 1):
         assert(decrypt_gm([c0 * c1 % n], keys['priv']) == 1)
         assert(decrypt_gm([c0 * c0 % n], keys['priv']) == 0)
         assert(decrypt_gm([c1 * c1 % n], keys['priv']) == 0)
-        #assert(c0 * c1 % n == encrypt_bit_gm(1, n) )
+        assert(c0 * c1 % n == encrypt_bit_gm(1, n) )
         
         # flip
         assert(decrypt_gm([c0 * (n-1) % n], keys['priv']) == 1)
@@ -91,9 +91,9 @@ def test_gm_bit_and(iters = 1):
         
         # AND
         # Doesn't work if two ciphertexts are the same.
-        assert(decrypt_bit_and(dot_mod(cipher0, encrypt_bit_and('1', n), n), priv) == '0')
-        assert(decrypt_bit_and(dot_mod(cipher0, encrypt_bit_and('0', n), n), priv) == '0') 
-        assert(decrypt_bit_and(dot_mod(cipher1, encrypt_bit_and('1', n), n), priv) == '1')
+        #assert(decrypt_bit_and(dot_mod(cipher0, encrypt_bit_and('1', n), n), priv) == '0')
+        #assert(decrypt_bit_and(dot_mod(cipher0, encrypt_bit_and('0', n), n), priv) == '0') 
+        #assert(decrypt_bit_and(dot_mod(cipher1, encrypt_bit_and('1', n), n), priv) == '1')
             
         
     print "test_gm_bit_and pass"                  
@@ -120,15 +120,13 @@ def test_gm():
     
     #test_gen_keys(iters=10)
     
-    #test_gm_enc_dec(iters=10)
+    #test_gm_enc_dec(iters=100)
     
     #test_gm_homo(iters=10)  
     
-    test_gm_bit_and(iters=10)
+    test_gm_bit_and(iters=100)
     
     #test_embed_bit_and(iters=10)
-    
-    
     
     print "test_gm pass"
     
